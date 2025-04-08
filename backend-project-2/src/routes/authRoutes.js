1c18830b8f82e7febaa4f71019d3b5729d8f0874
JayTheCoder77 <jayantiyer90@gmail.com>
2025-04-08 18:38:24 +0530

    8 april 2025

--- backend-project-2/src/routes/authRoutes.js
+++ backend-project-2/src/routes/authRoutes.js
@@ -59,7 +59,11 @@ router.post('/login',(req,res) => {
         // if password doesnt match return out
         if(!passwordIsValid) {return res.status(401).send({message : "Invalid Password"})}
 
+        console.log(user);
         // if all these passed we have successfull auth
+        const token = jwt.sign({id : user.id} , process.env.JWT_SECRET , {expiresIn : '24h'})
+        res.send({token})
+        
     } catch (error) {
         console.log(error.message);
         res.sendStatus(503)        
