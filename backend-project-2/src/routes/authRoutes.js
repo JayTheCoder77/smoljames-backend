d8a258eaf6ac0f21440eb8d5c716cc94b6f1f545
JayTheCoder77 <jayantiyer90@gmail.com>
2025-04-11 20:48:43 +0530

    done project todo sqlite

--- backend-project-2/src/routes/authRoutes.js
+++ backend-project-2/src/routes/authRoutes.js
@@ -5,70 +5,62 @@ import db from '../db.js'
 
 const router = express.Router()
 
-// register a new user /auth/register
-router.post('/register',(req,res) => {
-    const {username , password} = req.body
-    // save username and irreversibly encrypt password
-    // save jay@gmail.com | ajfjoifjaif
+// Register a new user endpoing /auth/register
+router.post('/register', (req, res) => {
+    const { username, password } = req.body
+    // save the username and an irreversibly encrypted password
+    // save gilgamesh@gmail.com | aklsdjfasdf.asdf..qwe..q.we...qwe.qw.easd
 
-    // encrypt password
-    const hashedPassword = bcrypt.hashSync(password,8)
+    // encrypt the password
+    const hashedPassword = bcrypt.hashSync(password, 8)
 
-    // save the new user and hased password to the db
+    // save the new user and hashed password to the db
     try {
-        const insertUser = db.prepare(`
-            INSERT INTO users(username,password) VALUES (? , ?)
-            `)        
-            const result = insertUser.run(username,hashedPassword)
+        const insertUser = db.prepare(`INSERT INTO users (username, password) VALUES (?, ?)`)
+        const result = insertUser.run(username, hashedPassword)
 
+        // now that we have a user, I want to add their first todo for them
+        const defaultTodo = `Hello :) Add your first todo!`
+        const insertTodo = db.prepare(`INSERT INTO todos (user_id, task) VALUES (?, ?)`)
+        insertTodo.run(result.lastInsertRowid, defaultTodo)
 
-            // creating a default todo as we have a new user 
-            const defaultTodo = `Hello !  :=) Add your first todo`
-            const insertTodo = db.prepare(`INSERT INTO todos (user_id , task) VALUES (? , ?)`)
-            insertTodo.run(result.lastInsertRowid , defaultTodo)
-
-            // added users and todo now we create a token
-            const token = jwt.sign({id:result.lastInsertRowid} , process.env.JWT_SECRET,{expiresIn : '24h'})
-            res.json({token})
-
-    } catch (error) {
-        console.log(error.message);
+        // create a token
+        const token = jwt.sign({ id: result.lastInsertRowid }, process.env.JWT_SECRET, { expiresIn: '24h' })
+        res.json({ token })
+    } catch (err) {
+        console.log(err.message)
         res.sendStatus(503)
     }
-    
 })
 
-router.post('/login',(req,res) => {
-    // we get email and look password asscociated with it in db
-    // but we get the encrypted password which means we cannot compare it to the one which the user entered
-    // so what we can do is again one way encrypt the password the user just entered 
+router.post('/login', (req, res) => {
+    // we get their email, and we look up the password associated with that email in the database
+    // but we get it back and see it's encrypted, which means that we cannot compare it to the one the user just used trying to login
+    // so what we can to do, is again, one way encrypt the password the user just entered
 
-    const {username,password} = req.body
+    const { username, password } = req.body
 
     try {
-        const getUser = db.prepare(`
-                SELECT * FROM users WHERE username = ?
-            `)
-
-        // if we cannot find user asscoiated with uesrname return out of function
+        const getUser = db.prepare('SELECT * FROM users WHERE username = ?')
         const user = getUser.get(username)
-        if(!user) {return res.status(404).send({message : 'user not found'})}
 
-        // compares our password with hashed password
+        // if we cannot find a user associated with that username, return out from the function
+        if (!user) { return res.status(404).send({ message: "User not found" }) }
+
         const passwordIsValid = bcrypt.compareSync(password, user.password)
-        // if password doesnt match return out
-        if(!passwordIsValid) {return res.status(401).send({message : "Invalid Password"})}
+        // if the password does not match, return out of the function
+        if (!passwordIsValid) { return res.status(401).send({ message: "Invalid password" }) }
+        console.log(user)
 
-        console.log(user);
-        // if all these passed we have successfull auth
-        const token = jwt.sign({id : user.id} , process.env.JWT_SECRET , {expiresIn : '24h'})
-        res.send({token})
-        
-    } catch (error) {
-        console.log(error.message);
-        res.sendStatus(503)        
+        // then we have a successful authentication
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '24h' })
+        res.json({ token })
+    } catch (err) {
+        console.log(err.message)
+        res.sendStatus(503)
     }
 
 })
+
 
 export default router
--- backend-project-2/src/server.js
+++ backend-project-2/src/server.js
@@ -1,35 +1,33 @@
 import express from 'express'
-import path, {dirname} from 'path'
+import path, { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import authRoutes from './routes/authRoutes.js'
 import todoRoutes from './routes/todoRoutes.js'
 import authMiddleware from './middleware/authMiddleware.js'
 
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5003
 
-// get file path from url of current module
+// Get the file path from the URL of the current module
 const __filename = fileURLToPath(import.meta.url)
-// get directory name from file path
+// Get the directory name from the file path
 const __dirname = dirname(__filename)
 
-
-// middleware
+// Middleware
 app.use(express.json())
-// serves html file from /public directory
-// tells express to serve all files from public folder as static assets/ file any requests for the css files will be resolved to the public directory
-app.use(express.static(path.join(__dirname,'../public')))
+// Serves the HTML file from the /public directory
+// Tells express to serve all files from the public folder as static assets / file. Any requests for the css files will be resolved to the public directory.
+app.use(express.static(path.join(__dirname, '../public')))
 
-// serving up the html file from /public directory
-app.get('/' , (req,res) => {
-    res.sendFile(path.join(__dirname,'public','index.html'))
-}) 
-
-// routes
-app.use('/auth',authRoutes)
-app.use('/todos',authMiddleware,todoRoutes)
-
-app.listen(PORT , () => {
-    console.log(`server started on PORT : ${PORT}`);
+// Serving up the HTML file from the /public directory
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
+// Routes
+app.use('/auth', authRoutes)
+app.use('/todos', authMiddleware, todoRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Server has started on port: ${PORT}`)
+})
